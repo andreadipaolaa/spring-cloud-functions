351336d0c646aac43ceeaabc31c62e3d8fbe0fe7
Andrea Di Paola <andrea.dipaola@outlook.com>
2023-05-19 10:32:35 +0200

    news

--- src/main/java/com/example/HelloHandler.java
+++ src/main/java/com/example/HelloHandler.java
@@ -1,22 +1,20 @@
 package com.example;
 
 import com.example.dto.PersonaDTO;
-import com.example.model.Greeting;
 import com.example.model.Persona;
 import com.example.model.PersonaService;
-import com.example.model.User;
 import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Optional;
 @Component
+@Log4j2
 public class HelloHandler {
 
     @Autowired
@@ -27,6 +25,7 @@ public class HelloHandler {
             @HttpTrigger(name = "request", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<PersonaDTO>> request,
             ExecutionContext context) {
         context.getLogger().info("insert new value");
+        log.debug("inserisco nuovo valore");
         PersonaDTO personaDTO = request.getBody().get();
         personaService.saveUser(new Persona(personaDTO.getName(),personaDTO.getEmail()));
         return request
@@ -40,6 +39,7 @@ public class HelloHandler {
             @HttpTrigger(name = "request", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             ExecutionContext context) {
         context.getLogger().info("prova di find");
+        log.debug("leggo tutti gli oggetti nel db");
         List<Persona> persone = personaService.findAll();
         return request
                 .createResponseBuilder(HttpStatus.OK)
